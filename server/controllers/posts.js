fde34282e30107ae0b3495bed7f7561920dd3c6b
Rafayyy <abdul.rafay@liqteq.com>
2023-02-14 13:08:00 +0500

    Posts App

---
+++ server/controllers/posts.js
@@ -0,0 +1,23 @@
+import postMessages from "../models/postMessage.js";
+
+export const getPosts = async (req, res) => {
+  try {
+    const postMessages = await postMessages.find();
+
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const createPost = async (req, res) => {
+  const body = req.body;
+  const newPost = new PostMessage(post);
+  try {
+    await newPost.save();
+
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
